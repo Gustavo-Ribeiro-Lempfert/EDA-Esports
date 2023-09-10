c6040dacf79dc446dea657cde8e692bfb8fcdc26
Gustavo Ribeiro <gustavoribe00@outlook.com>
2023-09-10 06:33:51 -0300

    Deletando coisas desnecessarias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Banner from './componentes/banner/banner';
 
@@ -6,20 +5,6 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
