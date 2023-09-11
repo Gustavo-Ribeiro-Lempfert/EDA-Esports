bb15e9ec4418ad664c0848c7cb084a49580d5db7
Gustavo Ribeiro <gustavoribe00@outlook.com>
2023-09-11 14:28:34 -0300

    Capturando dados do formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Formulario from './componentes/Formulario/Formulario';
 import Banner from './componentes/banner/banner';
 
 function App() {
+
+  const [jogadores, setJogador] = useState([])
+
+  const aoNovoJogadorAdicionado = (jogador) =>{
+    console.log(jogador);
+    setJogador([...jogadores, jogador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario aoJogadorCadastrado={jogador => aoNovoJogadorAdicionado(jogador)}/>
     </div>
   );
 }
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import Botao from '../Botao/Botao'
 import CampoTexto from '../CampoTexto/CampoTexto'
 import ListaSuspensa from '../ListaSuspensa/ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
+
 
     const posicoes = [
         'Top',
@@ -12,12 +14,43 @@ const Formulario = () => {
         'Atirador',
         'Suporte'
     ]
+
+    const [nome, setNome] = useState('')
+    const [posicao, setposicao] = useState('')
+    const [imagem, setimagem] = useState('')
+
+    const aoSAlvar = (evento) =>{
+        evento.preventDefault()
+        props.aoJogadorCadastrado({
+            nome,
+            posicao,
+            imagem
+        })
+    }
+
     return (
         <section className='formulario'>
-            <form>
-                <CampoTexto label="Nick/nome" placeholder='Nome "Nick" Sobrenome' />
-                <ListaSuspensa itens={posicoes} label="posicao" placeholder="Diga em qualposição o jogador deseja jogar"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem" />
+            <form onSubmit={aoSAlvar}>
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Nick/nome" 
+                placeholder='Nome "Nick" Sobrenome' 
+                valor={nome} 
+                aoAlterado={valor => setNome(valor)}
+                />
+                <ListaSuspensa 
+                itens={posicoes} 
+                label="posicao" 
+                placeholder="Diga em qualposição o jogador deseja jogar" 
+                valor={posicao} 
+                aoAlterado={valor => setposicao(valor)}
+                />
+                <CampoTexto 
+                label="Imagem" 
+                placeholder="Digite o endereço da sua imagem" 
+                valor={imagem} 
+                aoAlterado={valor => setimagem(valor)}
+                />
                 <Botao texto="Cadastrar jogador"/>
             </form>
         </section>
